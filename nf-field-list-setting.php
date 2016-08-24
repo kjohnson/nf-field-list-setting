c3aac60b311b39420bb8267cc3ac7db9f1e65f72
Kyle B. Johnson <kyle@wpninjas.com>
2016-08-24 14:44:45 -0400

    initial commit.

---
+++ nf-field-list-setting.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Plugin Name: Ninja Forms - Field List Setting
+ * Description: FOR TESTING ONLY. Example implementation of the Field List setting for testing.
+ * Version: 3.0
+ */
+
+final class NF_FieldListSetting
+{
+    public function __construct()
+    {
+        add_filter( 'ninja_forms_register_actions', array( $this, 'register_actions' ) );
+    }
+
+    public function register_actions( $actions )
+    {
+        require_once 'action.php';
+        $actions[ 'field-list-action' ] = new NF_FieldListSetting_Action();
+
+        return $actions;
+    }
+}
+
+new NF_FieldListSetting();
